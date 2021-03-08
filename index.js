b6b1915c06434f2c73f66e07162ffc74bb8949f6
Isa Abutaa <isaabutaa@gmail.com>
2021-03-08 12:50:06 -0500

    challenge reference

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Objective: Find first non-repeating character in a string. If all characters repeat, return '_'
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
 
 function firstNotRepeatingChar(str) {
     for(let i = 0; i < str.length; i++) {
