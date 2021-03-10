f401a889ae8f98f84186e3b3cf6ce01f594b0dff
Isa Abutaa <isaabutaa@gmail.com>
2021-03-10 10:59:04 -0500

    arr filter challenge and inheritance class challenge

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Objective: Find first non-repeating character in a string. If all characters repeat, return '_'
+// ***** Objective: Find first non-repeating character in a string. If all characters repeat, return '_'
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
 
 /* function firstNotRepeatingChar(str) {
@@ -11,56 +11,70 @@
     return '_'
 } */
 
-// Objective: Return the first duplicate number in an array. If no duplicate, return -1
+// ***** Objective: Return the first duplicate number in an array. If no duplicate, return -1
 // References: https://dev.to/seanwelshbrown/find-the-first-duplicate-in-a-javascript-array-5da3
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
 
 /* function firstDuplicate(a) {
     const numSet = new Set()
     for(let i = 0; i < a.length; i++) {
-        if(numSet.has(a[i])) return a[i]
-        numSet.add(a[i])
+        let num = a[i]
+        if(numSet.has(num)) return num
+        numSet.add(num)
     }
     return -1
 } */
 
-// Challenge: Create a Book class that takes a title, author, ISBN, and numCopies as arguments. Create a getAvailability, sell, and restock method.
+// ***** Challenge: Create a Book class that takes a title, author, ISBN, and numCopies as arguments. Create a getAvailability, sell, and restock method.
 
-/* class Book {
-    constructor(title, author, ISBN, numCopies) {
-        this.title = title;
-        this.author = author;
-        this.ISBN = ISBN;
-        this.numCopies = numCopies;
-    }
+// class Book {
+//     constructor(title, author, ISBN, numCopies) {
+//         this.title = title;
+//         this.author = author;
+//         this.ISBN = ISBN;
+//         this.numCopies = numCopies;
+//     }
 
-    getAvailability() {
-        if(this.numCopies == 0) return "Out of stock: No copies left";
-        else if(this.numCopies < 10) return `Low stock: ${this.numCopies} copies left`;
-        else return `In stock: ${this.numCopies} copies left`;
-    }
+//     getAvailability() {
+//         if(this.numCopies == 0) return "Out of stock: No copies left";
+//         else if(this.numCopies < 10) return `Low stock: ${this.numCopies} copies left`;
+//         else return `In stock: ${this.numCopies} copies left`;
+//     }
 
-    sell(n = 1) {
-        this.numCopies -= n;
-        if(this.numCopies == 1) return `Sold ${n}. You now have ${this.numCopies} copy in stock`;
-        return `Sold ${n}. You now have ${this.numCopies} copies in stock`;
-    }
+//     sell(n = 1) {
+//         this.numCopies -= n;
+//         if(this.numCopies == 1) return `Sold ${n}. You now have ${this.numCopies} copy in stock`;
+//         return `Sold ${n}. You now have ${this.numCopies} copies in stock`;
+//     }
 
-    restock(n = 5) {
-        this.numCopies += n;
-        return `Restocked ${n} more. You now have ${this.numCopies} copies in stock`;
-    }
+//     restock(n = 5) {
+//         this.numCopies += n;
+//         return `Restocked ${n} more. You now have ${this.numCopies} copies in stock`;
+//     }
 
-}
+// }
 
-const harryPotter = new Book("Harry Potter & the Sorceror's Stone", "J.K. Rowling", 09384509384, 10)
+// class TechnicalBook extends Book {
+//     constructor(title, author, ISBN, numCopies, edition) {
+//         super(title, author, ISBN, numCopies);
+//         this.edition = edition;
+//     }
 
-console.log(harryPotter.getAvailability());
-console.log(harryPotter.sell(10));
-console.log(harryPotter.restock()); */
+//     getEdition() {
+//         return `The edition of this technical book is ${edition} edition`;
+//     }
+// }
 
+// const harryPotter = new Book("Harry Potter & the Sorceror's Stone", "J.K. Rowling", 09384509384, 10)
 
-// Challenge: Create a Movie object using a constructor function or class. Write a getOverview method that returns the properties of the movie.
+// console.log(harryPotter.getAvailability());
+// console.log(harryPotter.sell(10));
+// console.log(harryPotter.restock()); 
+
+// const codingBook = new TechnicalBook("essential JS", "author name", 90835039, 10, "5th")
+// console.log(codingBook.sell())
+
+// ***** Challenge: Create a Movie object using a constructor function or class. Write a getOverview method that returns the properties of the movie.
 
 /* function Movie(title, director, genre, releaseYear, rating) {
     this.title = title;
@@ -92,3 +106,36 @@ console.log(sevenPounds.getOverview()); */
 }
 const sevenPounds = new Movie("Seven Pounds", "directorName", "drama", 2007, 5);
 console.log(sevenPounds.getOverview()); */
+
+// ***** Challenge: Given an array of dishes, filter through them to return the ones that are vegetarian. For each vegetarian dish, dynamically render them to the DOM in a list
+/* 
+const dishes = [
+    {
+        name: "Eggplant Parmesan",
+        vegetarian: true
+    },
+    {
+        name: "Spaghetti with Meatballs",
+        vegetarian: false
+    },
+    {
+        name: "Margherita Pizza",
+        vegetarian: true
+    },
+    {
+        name: "Mom's Lasagna",
+        vegetarian: false
+    }
+]
+
+const vegetarianMenu = document.querySelector("#vegetarian-list")
+const vegetarianDishes = dishes.filter(dish => dish.vegetarian)
+const displayItems = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        const listItem = document.createElement("li")
+        listItem.textContent = `Dish: ${arr[i].name}`
+        vegetarianMenu.append(listItem)
+    }
+}
+
+displayItems(vegetarianDishes) */
